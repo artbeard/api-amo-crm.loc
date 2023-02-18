dc51c5975ced26b6cbf1e9a73c7169bccf387415
artbeard <frolov.web@gmail.com>
2023-02-18 23:33:54 +0600

    добавлен axios

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from "@nestjs/axios";
 
 @Injectable()
-export class ApiService {}
+export class ApiService {
+	constructor(
+		private readonly httpService: HttpService
+	) {}
+
+	//return this.httpService.axiosRef.get('http://.../api/v4/..');
+
+}
