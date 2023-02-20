3295bdd97704315152ae83f4fbc92b471d8c64de
artbeard <frolov.web@gmail.com>
2023-02-21 00:41:36 +0600

    почистил мусор

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,18 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
-import { HttpModule, HttpService } from "@nestjs/axios";
-import { AuthService } from "../auth/auth.service";
+import { HttpModule } from "@nestjs/axios";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
 	controllers: [ApiController],
 	providers: [
-	//	HttpService,
 		ApiService,
-		//AuthService,
 	],
 	imports: [
-		HttpModule
+		HttpModule,
+		AuthModule
 	],
 })
 export class ApiModule {}
