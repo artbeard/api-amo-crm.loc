337db978f3c81a6b699694b124ecd2ec65353ffc
artbeard <76746834+artbeard@users.noreply.github.com>
2023-02-20 17:59:46 +0600

    вынес обращение к базе в отдельный метод

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -7,8 +7,6 @@ export class ApiService {
 		private readonly httpService: HttpService
 	) {}
 
-	//return this.httpService.axiosRef.get('http://.../api/v4/..');
-
 	getAuthParams(apiKey: string): Promise<any>
 	{
 		return this.httpService.axiosRef.get('https://test.gnzs.ru/oauth/get-token.php', {
@@ -26,32 +24,41 @@ export class ApiService {
 
 	}
 
+	private entityTypes = {
+		deal: 'leads',
+		contact: 'contacts',
+		company: 'companies'
+	}
+
 	createCompany(companyName: string, host: string, token: string): Promise<any>
 	{
-		return this.httpService.axiosRef.post(`https://${host}/api/v4/companies`,
-			{
-				name: companyName,
-				request_id: companyName + 'create'
-			},
+		return this.createEntity('company', companyName, host, token);
+	}
+
+	private createEntity(entityType:string, entityName: string, host: string, token: string): Promise<any>
+	{
+		return this.httpService.axiosRef.post(`https://${host}/api/v4/${this.entityTypes[entityType as 'company' | 'contact' | 'deal' ]}`,
+			[{
+				name: entityName,
+				request_id: entityName + 'create' //
+			}],
 			{
 				headers: {
 					'Content-Type': 'application/json',
 					'Authorization': `Bearer ${token}`
 				}
-		})
+			})
 			.then(resp => {
 				return resp.data;
 			})
 			.catch(err => {
-				//throw
-				console.log(err.response.status)
 				if (err.response.status === 401)
 				{
 					throw new HttpException('Вы неавторизованы', HttpStatus.UNAUTHORIZED);
 				}
 				else if (err.response.status === 400)
 				{
-					throw new HttpException('Ошибка', HttpStatus.UNAUTHORIZED);
+					throw new HttpException(err.response?.data?.detail ?? 'Ошибка валидации', HttpStatus.BAD_REQUEST);
 				}
 				else
 				{
@@ -59,4 +66,5 @@ export class ApiService {
 				}
 			})
 	}
+
 }
